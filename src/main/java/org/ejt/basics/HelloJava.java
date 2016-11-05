21c5c5b40cb68b3ed34f6c8f0cfece16df6e3f7b
EasyJavaTutorials <easyjavatutorials@gmail.com>
2016-11-05 16:57:51 -0700

    Initial commit to java project.

---
+++ src/main/java/org/ejt/basics/HelloJava.java
@@ -0,0 +1,20 @@
+package org.ejt.basics;
+
+/**
+ * Created by http://easyjavatutorials.in on 11/5/16.
+ * <p>
+ * A place to find easy java tutorials written by developers for developers.
+ * <p>
+ * Released under :
+ * <p>
+ * Apache License 2.0: http://www.apache.org/licenses/LICENSE-2.0.txt
+ * <p>
+ * Happy Java Coding.
+ */
+public class HelloJava {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Java World.");
+    }
+
+}
